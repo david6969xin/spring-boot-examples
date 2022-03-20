8675b727d82b8156a47ea15890c34a1ea18aa6ee
david6969xin <86217945+david6969xin@users.noreply.github.com>
2022-03-20 13:51:12 +0800

    helloWorld and scheduler (#1)

--- spring-boot-scheduler/src/main/java/com/neo/task/Scheduler2Task.java
+++ spring-boot-scheduler/src/main/java/com/neo/task/Scheduler2Task.java
@@ -1,23 +1,19 @@
 package com.neo.task;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-/**
- * Created by summer on 2016/12/1.
- */
+import java.time.LocalDateTime;
 
 @Component
 public class Scheduler2Task {
 
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-
-    @Scheduled(fixedRate = 6000)
+    @Async
+    @Scheduled(fixedRate = 1000)
     public void reportCurrentTime() {
-        System.out.println("现在时间：" + dateFormat.format(new Date()));
+        System.out.println("Scheduler2Task ==> " +
+                LocalDateTime.now().toLocalTime() + " ===> " + Thread.currentThread().getName());
     }
 
 }
--- spring-boot-scheduler/src/test/java/com/neo/SchedulerApplicationTests.java
+++ spring-boot-scheduler/src/test/java/com/neo/SchedulerApplicationTests.java
@@ -1,17 +1,18 @@
 package com.neo;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import java.util.concurrent.TimeUnit;
+
+
 @SpringBootTest
 public class SchedulerApplicationTests {
 
 	@Test
-	public void contextLoads() {
-		System.out.println("hello world");
+	public void contextLoads() throws InterruptedException {
+		System.out.println("scheduler tasks are running");
+		TimeUnit.SECONDS.sleep(10);
 	}
 
 }
